14ab368dffd8d9879aacd910cfbaa0c194e33285
ruggab <ruggzan@gmail.com>
2021-07-22 16:27:33 +0200

    Primo Commit

---
+++ src/main/java/net/smart/rfid/WMSBatch/WmsBatchApplication.java
@@ -0,0 +1,13 @@
+package net.smart.rfid.WMSBatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmsBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/smart/rfid/WMSBatch/WmsBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package net.smart.rfid.WMSBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
