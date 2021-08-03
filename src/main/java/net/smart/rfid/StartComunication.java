4a498a62cc8a8d84a57d64cad63ab66d3b0b1dcd
Gabriele <ruggzan@gmail.com>
2021-08-03 07:57:24 +0200

    aggiunto web api

--- src/main/java/net/smart/rfid/StartComunication.java
+++ src/main/java/net/smart/rfid/StartComunication.java
@@ -3,8 +3,6 @@ package net.smart.rfid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
---
+++ src/main/java/net/smart/rfid/controller/WMSController.java
@@ -0,0 +1,27 @@
+package net.smart.rfid.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.smart.rfid.jobs.WMS;
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:4200")
+public class WMSController {
+
+	
+	@PostMapping("/callWMSIn")
+	public String  callWMSIn(@RequestBody String packageId) throws Exception {
+		try {
+			WMS.barcodeIn = packageId;
+			
+			return "ok";
+		} catch (Exception e) {
+			throw e;
+		}
+
+	}
+}
