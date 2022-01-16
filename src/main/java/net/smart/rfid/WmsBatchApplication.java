2d1b79b97af0764c3a103f282ec73532d200ab95
Gabriele <ruggzan@gmail.com>
2022-01-16 20:51:19 +0100

    comento

--- src/main/java/net/smart/rfid/WmsBatchApplication.java
+++ src/main/java/net/smart/rfid/WmsBatchApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WmsBatchApplication {
 	
-	
+	/*Questo batch è sempre in ascolto dei messaggi che arrivano dal barcode del WMS Morato per il tunnel automatico
+	 *dopo aver effettuato la login il barcode manda viene inviato con un messaggio di tipo CC in tal caso il batch memorizza l'atteso
+	 *se il messaggio è di tipo EC sarà inviato l'esito */
    
 	public static void main(String[] args) {
 		SpringApplication.run(WmsBatchApplication.class, args);
