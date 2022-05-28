078bbf4a8ae43087e11585723d2c0440250dac42
Gabriele <ruggzan@gmail.com>
2022-05-28 09:09:57 +0200

    modifiche per risoluzione problema concomitanza tomcat

--- src/main/java/net/smart/rfid/StartComunication.java
+++ src/main/java/net/smart/rfid/StartComunication.java
@@ -1,54 +1,46 @@
-package net.smart.rfid;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-
-import net.smart.rfid.controller.WMSController;
-import net.smart.rfid.jobs.WMSJob;
-import net.smart.rfid.tunnel.db.services.DataStreamService;
-
-@Component
-public class StartComunication implements CommandLineRunner {
-
-	private static final Logger logger = LoggerFactory.getLogger(StartComunication.class);
-
-	@Autowired
-	DataStreamService dataStreamService;
-
-	@Override
-	public void run(String... args) throws Exception {
-
-	
-		Client client = ClientBuilder.newClient();
-	
-		WebTarget target = client.target("http://localhost:8080/api/v1/startWms");
-		try {
-			target.request().get();
-		} catch (Exception e) {
-			logger.error("Path error");
-			target = client.target("http://localhost:8080/WMSBatch/api/v1/startWms");
-			try {
-				target.request().get();
-			} catch (Exception e2) {
-				logger.error("Path error 2");
-			}
-			
-		}
-		
-		// logger.info("Start Thread WMSAuto");
-		//
-		// WMSJob wmsAuto = new WMSJob(dataStreamService);
-		// Thread threadWms = new Thread(wmsAuto);
-		// threadWms.start();
-	}
-
-}
+//package net.smart.rfid;
+//
+//import javax.ws.rs.client.Client;
+//import javax.ws.rs.client.ClientBuilder;
+//import javax.ws.rs.client.WebTarget;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//
+//import net.smart.rfid.tunnel.db.services.DataStreamService;
+//
+//@Component
+//public class StartComunication implements ApplicationRunner {
+//
+//	private static final Logger logger = LoggerFactory.getLogger(StartComunication.class);
+//
+//	@Autowired
+//	DataStreamService dataStreamService;
+//
+//	@Override
+//	public void run(ApplicationArguments args) throws Exception {
+//
+//		Client client = ClientBuilder.newClient();
+//
+//		WebTarget target = client.target("http://localhost:8080/api/v1/startWms");
+//		try {
+//			target.request().get();
+//		} catch (Exception e) {
+//			logger.error("Path error");
+//			target = client.target("http://localhost:8080/WMSBatch/api/v1/startWms");
+//			try {
+//				target.request().get();
+//			} catch (Exception e2) {
+//				logger.error("Path error 2");
+//			}
+//
+//		}
+//
+//	}
+//
+//}
--- src/main/java/net/smart/rfid/WmsBatchApplication.java
+++ src/main/java/net/smart/rfid/WmsBatchApplication.java
@@ -2,20 +2,28 @@ package net.smart.rfid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 public class WmsBatchApplication extends SpringBootServletInitializer {
-	
-	/*Questo batch è sempre in ascolto dei messaggi che arrivano dal barcode del WMS Morato per il tunnel automatico
-	 *dopo aver effettuato la login il barcode manda viene inviato con un messaggio di tipo CC in tal caso il batch memorizza l'atteso
-	 *se il messaggio è di tipo EC sarà inviato l'esito */
-   
-	public static void main(String[] args) {
-		SpringApplication.run(WmsBatchApplication.class, args);
-		
-	}
-	
-	
 
-} 
+	/*
+	 * Questo batch è sempre in ascolto dei messaggi che arrivano dal barcode del WMS Morato per il tunnel automatico
+	 * dopo aver effettuato la login il barcode manda viene inviato con un messaggio di tipo CC in tal caso il batch
+	 * memorizza l'atteso se il messaggio è di tipo EC sarà inviato l'esito
+	 */
+
+	public static void main(String[] args) {
+		SpringApplication.run(applicationClass, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<WmsBatchApplication> applicationClass = WmsBatchApplication.class;
+
+}
